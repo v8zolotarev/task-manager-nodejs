ed2180772099782acd8591a99de0ba198c7d21b7
v8zolotarev <vadim8zolotarev@gmail.com>
2024-12-19 02:00:44 +0300

    Finished first NodeJS app

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 app.use(express.json());
-
-
-
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
-})
+app.use(express.static('./public'));
 
 app.use('/app/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on ${port}...`))
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGODB_URI);
+        app.listen(port, console.log(`server is listening on ${port}...`))
+    } catch (error){
+        console.log(error)
+    }
+}
+
+start()
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-res.send('all items')
-})
+const {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/tasks');
+
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 
 module.exports = router;
